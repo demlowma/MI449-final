4dc6e85d4942cac1667876fad3eb9deaf111241e
Maddie Demlow <demlowma@msu.edu>
2023-04-19 13:28:56 -0400

    test deploy commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - Final Project MI 449!
         </a>
       </header>
     </div>
